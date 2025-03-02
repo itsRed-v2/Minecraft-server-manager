41f5ae6869d7b13682c5003741406ba5214a9e3d
itsRed-v2 <paul.charignon.v2@gmail.com>
2025-03-02 18:37:58 +0000

    Vérification du conflit de port avant de démarrer un serveur

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -14,29 +14,45 @@ const data = new SlashCommandBuilder()
 async function execute(interaction: ChatInputCommandInteraction, servers: MinecraftServer[]) {
     requireServerArgument(interaction, servers, async (server) => {
 
+        // Permission checking
         if (!server.isPublic && !isAdministrator(interaction.user.id)) {
             interaction.reply(embeds.fail("Vous n'avez pas la permission de démarrer ce serveur."));
             return;
         }
 
+        // Do not start server if already running
         if (server.state != 'Stopped') {
             interaction.reply(embeds.fail("On ne peut pas démarrer un serveur qui n'est pas à l'arrêt."));
             return;
         }
 
+        // Running server limit
         let runningPublicServers = 0;
         for (const server of servers) {
             if (server.isPublic && server.state !== 'Stopped') {
                 runningPublicServers++;
             }
         }
-
         if (runningPublicServers >= 2) {
             interaction.reply(embeds.fail("Trop de serveurs sont allumés en même temps, "
                 + "arrêtez en un pour en démarrer un autre."));
             return;
         }
+
+        // Port conflict checking
+        for (const otherServer of servers) {
+            if (server === otherServer) continue;
+
+            if (otherServer.state !== 'Stopped' && server.port === otherServer.port) {
+                interaction.reply(embeds.fail(`Le serveur **${otherServer.name}** est `
+                    + `déjà démarré à l'adresse de **${server.name}**. `
+                    + "Impossible de démarrer en même temps deux serveurs qui ont "
+                    + "la même adresse."));
+                return;
+            }
+        }
     
+        // Finally starting the server
         const success = await server.start();
         if (success) {
             interaction.reply(embeds.success(`✅ Démarrage de **${server.name}**.`));
